31f89c41ec05fa0df2c96d41702dd95727c5c4e5
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-03 20:11:56 +0600

    header section created and real time added with moment js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
+import moment from 'moment';
 
 const Header = () => {
-    return (
-        <div>
-            <h3>this is header</h3>
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <img src="/src/assets/logo.png" alt="" />
+      </div>
+      <p className="text-[#706F6F] text-center ">Journalism Without Fear or Favour</p>
+      <p className="text-xl text-center">{moment().format("dddd, MMMM D , YYYY")}</p>
+    </div>
+  );
 };
 
 export default Header;
