316ce1e4d839fd2deb05e633a8d096f75b93b2d9
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-05 15:49:13 +0600

    right sidebar login with method displayed conditionally

--- src/components/RightSideNav/LoginWith.jsx
+++ src/components/RightSideNav/LoginWith.jsx
@@ -1,14 +1,34 @@
-import {FcGoogle} from 'react-icons/fc'
-import {BsGithub} from 'react-icons/bs'
+import { FcGoogle } from "react-icons/fc";
+import { BsGithub } from "react-icons/bs";
+import { useContext } from "react";
+import { AuthContext } from "../../Authentication/AuthProvider";
 
 const LoginWith = () => {
-    return (
-        <div className='space-y-2'>
-            <h3 className="text-xl font-bold mb-4" >Login with</h3>
-            <button className="btn btn-wide capitalize btn-outline btn-info"> <FcGoogle className='text-xl'></FcGoogle> Login With Google</button>
-            <button className="btn btn-wide capitalize btn-outline btn-netural"> <BsGithub className='text-xl'></BsGithub> Login With Github</button>
-        </div>
-    );
+  const { user, loading } = useContext(AuthContext);
+
+  return (
+    <div>
+      {loading ? (
+        ""
+      ) : user ? (
+        ""
+      ) : (
+        <>
+          <div className="space-y-2">
+            <h3 className="text-xl font-bold mb-4">Login with</h3>
+            <button className="btn btn-wide capitalize btn-outline btn-info">
+              {" "}
+              <FcGoogle className="text-xl"></FcGoogle> Login With Google
+            </button>
+            <button className="btn btn-wide capitalize btn-outline btn-netural">
+              {" "}
+              <BsGithub className="text-xl"></BsGithub> Login With Github
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default LoginWith;
